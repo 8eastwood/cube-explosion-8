33a4dc95860977a94bc7123f83b188a66c100555
Egor <m.fishborn@yandex.ru>
2024-06-12 11:08:27 +0300

    init

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Spawner))]
+[RequireComponent(typeof(Exploder))]
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _cube;
+    [SerializeField] private Color[] _colors;
+
+    private Spawner _spawner;
+    private Exploder _exploder;
+
+    private void Awake()
+    {
+        _exploder = GetComponent<Exploder>();
+        _spawner = GetComponent<Spawner>();
+    }
+
+    private void OnMouseDown()
+    {
+        SpawnDividedObjects();
+    }
+
+    public void SetRandomColor(MeshRenderer meshRenderer)
+    {
+        int minColorNumber = 0;
+        int randomColor = Random.Range(minColorNumber, _colors.Length);
+        meshRenderer.material.color = _colors[randomColor];
+    }
+
+    private void SpawnDividedObjects()
+    {
+        int minDivideChance = 0;
+        int maxDivideChance = 100;
+
+        int randomChanceToDivide = Random.Range(minDivideChance, maxDivideChance);
+
+        if (randomChanceToDivide <= _spawner._chanceToDivide)
+        {
+            _spawner.SpawnObjects();
+        }
+        else
+        {
+            _exploder.Explode();
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+
+class Spawner : MonoBehaviour
+{
+    [SerializeField] public int _chanceToDivide = 100;
+    [SerializeField] private int _minInstantiateNumber = 2;
+    [SerializeField] private int _maxInstantiateNumber = 6;
+
+    public MeshRenderer _meshRenderer;
+    private Cube _cube;
+    private int _randomInstantiateAmount;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SpawnObjects()
+    {
+        float scaleMultiplier = 0.5f;
+        int divider = 2;
+
+        _randomInstantiateAmount = Random.Range(_minInstantiateNumber, _maxInstantiateNumber + 1);
+        transform.localScale *= scaleMultiplier;
+        _chanceToDivide /= divider;
+
+        for (int i = 0; i < _randomInstantiateAmount; i++)
+        {
+            MeshRenderer renderer = Instantiate(_meshRenderer, transform.position, Quaternion.identity);
+            _cube.SetRandomColor(renderer);
+        }
+    }
+}
